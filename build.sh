33799a7e818d42aeb81f7a2a514e1dffff9e2c16
Montana Mendy <montana@getprowl.com>
2021-01-25 03:36:35 -0800

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -x
-
 echo "$ROBOT_PASSWORD" | docker login -u "$ROBOT_USERNAME" --password-stdin quay.io
-docker push quay.io/montana/multi-arch-travis
+docker pull quay.io/montana/multi-arch-travis
