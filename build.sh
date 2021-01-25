6483f308cd205b228b4b64f4e95085fd590d54f3
Montana Mendy <montana@getprowl.com>
2021-01-25 03:24:00 -0800

    add shebang line

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -x
 
-ARCH=$(uname -m)
-
-LD_DEBUG=libs docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
-docker tag container/$ARCH quay.io/montana/multi-arch-travis:$ARCH
-docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-docker push quay.io/montana/multi-arch-travis:$ARCH
+echo "$ROBOT_PASSWORD" | docker login -u "$ROBOT_USERNAME" --password-stdin quay.io
+docker push quay.io/montana/multi-arch-travis
